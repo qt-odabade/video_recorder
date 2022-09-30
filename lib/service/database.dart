dd6c5f0eb1544b326a78a89a36d05baa9cabe062
Omkar Dabade <omkar.dabade@quantello.com>
2022-09-30 18:02:32 +0530

    added singleton db

--- lib/service/database.dart
+++ lib/service/database.dart
@@ -3,6 +3,12 @@ import 'package:video_recorder/model/video_file.dart';
 import '../objectbox.g.dart';
 
 class Database {
+  static final Database _database = Database._internal();
+
+  Database._internal();
+
+  static Database get instance => _database;
+
   late Box<VideoFile> _box;
 
   Future<void> init() async {
